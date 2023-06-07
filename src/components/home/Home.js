189d76da238c80eb4bbe30d6a1a67543d3fe2242
aleikson <aleiksonsilva@hotmail.com>
2023-06-07 09:24:47 -0300

    Added home

---
+++ src/components/home/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Style from './Home.module.css';
+import Contact from '../contactMe/Contact';
+
+const Home = () => {
+  return (
+    <div className={Style.home}>
+      <div className={Style.subtitle}>
+        <div className={Style.title}>
+          <h2>Olá, eu sou o Aleikson</h2>
+          <Contact />
+        </div>
+        <div className={Style.role}>
+          <h1>FRONTEND</h1>
+        </div>
+        <div className={Style.role2}>
+          <h1>DEVELOPER</h1>
+        </div>
+        <div className={Style.description}>
+          <p>
+            Tenho experiência em criar interfaces interativas e funcionais, entregando soluções de alta qualidade e usabilidade.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
