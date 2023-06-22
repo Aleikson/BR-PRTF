635105f15cdb3b49a6172dc557a28d5b449b3274
aleikson <aleiksonsilva@hotmail.com>
2023-06-22 17:42:24 -0300

    Responsive menu

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,57 +1,81 @@
 import React, { useEffect, useState } from 'react';
 import style from './NavBar.module.css';
 
-
 const NavBar = () => {
-    const [name, setName] = useState('');
+  const [name, setName] = useState('');
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
-    useEffect(() => {
-        const targetName = "< Home />";
-        let currentIndex = 0;
-        let isDeleting = false;
+  useEffect(() => {
+    const targetName = '< Home />';
+    let currentIndex = 0;
+    let isDeleting = false;
 
-        const animateTyping = () => {
-            const currentName = targetName.slice(0, currentIndex);
+    const animateTyping = () => {
+      const currentName = targetName.slice(0, currentIndex);
 
-            setName(currentName);
+      setName(currentName);
 
-            if (!isDeleting) {
-                currentIndex++;
+      if (!isDeleting) {
+        currentIndex++;
 
-                if (currentIndex > targetName.length) {
-                    isDeleting = true;
-                    currentIndex = targetName.length;
-                }
-            } else {
-                currentIndex--;
+        if (currentIndex > targetName.length) {
+          isDeleting = true;
+          currentIndex = targetName.length;
+        }
+      } else {
+        currentIndex--;
 
-                if (currentIndex === 0) {
-                    isDeleting = false;
-                    currentIndex = 0;
-                }
-            }
-        };
+        if (currentIndex === 0) {
+          isDeleting = false;
+          currentIndex = 0;
+        }
+      }
+    };
 
-        const intervalId = setInterval(animateTyping, 250);
+    const intervalId = setInterval(animateTyping, 250);
 
-        return () => {
-            clearInterval(intervalId);
-        };
-    }, []);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
-    return (
-        <div className={style.container}>
-            <a href="/" className={style.nameLink}>
-                {name}
-            </a>
-            <div className={style.links}>
-                <a href="experience">Experiência</a>
-                <a href="perfil">Sobre</a>
-                <a href="skills">Skills</a>
-                <a href="portfolio">Portfolio</a>
-            </div>
-        </div>
-    );
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  const handleLinkClick = (e) => {
+    e.preventDefault();
+
+    const targetId = e.target.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: 'smooth',
+      });
+    }
+
+    setMenuOpen(false);
+  };
+
+  return (
+    <div className={style.container}>
+      <a href="/" className={style.nameLink}>
+        {name}
+      </a>
+      <div className={`${style.links} ${isMenuOpen ? style.open : ''}`}>
+        <a href="#perfil" onClick={handleLinkClick}>Sobre</a>
+        <a href="#skills" onClick={handleLinkClick}>Skills</a>
+        <a href="#experience" onClick={handleLinkClick}>Experiência</a>
+      </div>
+      <div className={style.menuIcon} onClick={toggleMenu}>
+        <span className={`${style.iconBar} ${isMenuOpen ? style.open : ''}`} />
+        <span className={`${style.iconBar} ${isMenuOpen ? style.open : ''}`} />
+        <span className={`${style.iconBar} ${isMenuOpen ? style.open : ''}`} />
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
